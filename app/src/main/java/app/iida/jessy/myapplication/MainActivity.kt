4dab627d4a2210dfcd310f2f7eb1f9a4bed14a2f
Jeccy-iida <drrr_drrr_4242@docomo.ne.jp>
2021-02-07 10:58:49 +0900

    初めてのコミット

---
+++ app/src/main/java/app/iida/jessy/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package app.iida.jessy.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+            button.text = "クリックされました！"
+        }
+    }
+}
